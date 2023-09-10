9f9ab2c7ddf72b01b44b2590f40df98529d2d816
¨Thiago <thiago.cancellarich@gmail.com>
2023-09-10 15:59:02 -0300

    Creando componentes

--- server/src/controllers/country/getCountries.js
+++ server/src/controllers/country/getCountries.js
@@ -10,7 +10,7 @@ const getCountries = async (req, res) => {
     // console.log(data.length)
 let array = [];
 const db = await Country.findAll();
-console.log(db);
+// console.log(db);
 
 for(let i = 0; i < data.length; i++) {
     const countries = {
@@ -26,16 +26,6 @@ for(let i = 0; i < data.length; i++) {
     array.push(countries)
     if(!db){
     await Country.create(countries)
-//     await Country.create({
-//     id:data[i]?.cca3,
-//     name:data[i]?.name?.common,
-//     flagsImage:data[i]?.flags?.png,
-//     continent:data[i]?.continents[0],
-//     capital:data[i].capital?data[i].capital[0]: "Empty",
-//     subRegion:data[i]?.subregion,
-//     area:data[i]?.area,
-//     population:data[i]?.population,
-// })
 }}
 
        res.status(200).json(array);
