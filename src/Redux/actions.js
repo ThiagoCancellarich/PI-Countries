0fee89a7749b1ab62e61e81b79bf0fdd153fe82e
ThiagoCancellarich <131214817+ThiagoCancellarich@users.noreply.github.com>
2023-11-15 12:41:36 -0300

    deploy front

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -1,17 +1,20 @@
 import axios from 'axios';
 import { GET_COUNTRIES, GET_COUNTRY_BY_NAME, GET_COUNTRY_BY_ID, POST_ACTIVITIES, FILTER_CONTINENT, FILTER_POPULATION, FILTER_ORDER, FILTER_ACTIVITIES, GET_ACTIVITIES } from './action-types';
 
+
+const VITE_URL = import.meta.env.VITE_URL
+
 //* funcion para traer toda la info de la base de datos
 export const getCountries = () =>{
     return async (dispatch) => {
-        const { data } = await axios.get('http://localhost:3001/countries');
+        const { data } = await axios.get(`${VITE_URL}/countries`);
         return dispatch({type:GET_COUNTRIES, payload: data });        
     }
 };
 //* funcion para traer un pais recibido por query
 export const getByName = (name) => {
     return async (dispatch) => {
-        const { data } = await axios.get(`http://localhost:3001/countries/name?name=${name}`);
+        const { data } = await axios.get(`${VITE_URL}/countries/name?name=${name}`);
         
         return dispatch ({type:GET_COUNTRY_BY_NAME, payload: data});
     }
@@ -19,7 +22,7 @@ export const getByName = (name) => {
 //* funcion para traer un pais recibido por params
 export const getCountryById = (id) => {
     return async (dispatch) => {
-    const { data } = await axios.get(`http://localhost:3001/countries/${id}`);
+    const { data } = await axios.get(`${VITE_URL}/countries/${id}`);
    
     
     return dispatch({type:GET_COUNTRY_BY_ID, payload: data});
@@ -28,7 +31,7 @@ export const getCountryById = (id) => {
 
 export const postActivities = (activity) => { 
     return async (dispatch) => {
-        const { data } = await axios.post(`http://localhost:3001/activity`, activity);
+        const { data } = await axios.post(`${VITE_URL}/activity`, activity);
         return dispatch({type:POST_ACTIVITIES, payload: data });
 
     }
@@ -36,7 +39,7 @@ export const postActivities = (activity) => {
 
 export const getActivities = () => {
     return async (dispatch) => {
-        const { data } = await axios.get('http://localhost:3001/activity')
+        const { data } = await axios.get(`${VITE_URL}/activity`)
         return dispatch({type:GET_ACTIVITIES, payload: data});
     }
 }
